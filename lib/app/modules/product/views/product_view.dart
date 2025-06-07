3d991f3eb410fdba2281560587863035df174084
Wachid Amirul M <contact@wachidamirul.com>
2025-06-07 21:46:07 +0700

    refactor: update product view layout; replace ListTile with custom GestureDetector and enhance item display

--- lib/app/modules/product/views/product_view.dart
+++ lib/app/modules/product/views/product_view.dart
@@ -6,7 +6,6 @@ import '../../../utils/constants/colors.dart';
 import '../../../utils/constants/sizes.dart';
 import '../../../utils/helpers/helper_functions.dart';
 import '../../../widgets/appbar/app_bar_blank.dart';
-import '../../../widgets/custom_shapes/containers/rounded_container.dart';
 import '../../../widgets/custom_shapes/containers/rounded_image.dart';
 import '../controllers/product_controller.dart';
 
@@ -33,6 +32,7 @@ class ProductView extends GetView<ProductController> {
         padding: const EdgeInsets.all(MySizes.md),
         itemBuilder:
             (_, index) => Container(
+              padding: const EdgeInsets.all(MySizes.sm),
               decoration: BoxDecoration(
                 color: dark ? MyColors.black : MyColors.white,
                 borderRadius: BorderRadius.circular(MySizes.cardRadiusMd),
@@ -44,85 +44,40 @@ class ProductView extends GetView<ProductController> {
                   ),
                 ],
               ),
-              child: ListTile(
-                leading: MyRoundedImage(
-                  width: 64,
-                  height: 64,
-                  isNetworkImage: true,
-                  imageUrl:
-                      "https://www.converse.id/media/catalog/product/0/1/01-CONVERSE-FFSSBCONA-CONA04144C-Black.jpg",
-                  applyImageRadius: true,
-                  borderRadius: MySizes.cardRadiusMd,
-                ),
-                title: Text(
-                  'Converse Chuck Taylor All Star',
-                  style: Theme.of(context).textTheme.titleLarge,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              child: GestureDetector(
+                onTap: () {
+                  // Action for the product item tap
+                  print("Product item tapped: $index");
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    const SizedBox(height: MySizes.spaceBtwItems / 2),
-                    Text(
-                      'Size:',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
+                    MyRoundedImage(
+                      width: 60,
+                      height: 60,
+                      isNetworkImage: true,
+                      imageUrl:
+                          "https://www.converse.id/media/catalog/product/0/1/01-CONVERSE-FFSSBCONA-CONA04144C-Black.jpg",
+                      applyImageRadius: true,
+                      borderRadius: MySizes.cardRadiusMd,
                     ),
-                    Wrap(
-                      spacing: MySizes.xs,
-                      runSpacing: MySizes.xs,
-                      children: [
-                        MyRoundedContainer(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: MySizes.sm,
-                            vertical: MySizes.xs,
-                          ),
-                          backgroundColor: MyColors.success,
-                          child: Text(
-                            '39: 10 pcs',
-                            style: TextStyle(
-                              color: MyColors.white,
-                              fontSize: 10,
-                            ),
-                          ),
-                        ),
-                        MyRoundedContainer(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: MySizes.sm,
-                            vertical: MySizes.xs,
-                          ),
-                          backgroundColor: MyColors.warning,
-                          child: Text(
-                            '40: 5 pcs',
-                            style: TextStyle(
-                              color: MyColors.white,
-                              fontSize: 10,
-                            ),
-                          ),
-                        ),
-                        MyRoundedContainer(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: MySizes.sm,
-                            vertical: MySizes.xs,
-                          ),
-                          backgroundColor: MyColors.error,
-                          child: Text(
-                            '41: 2 pcs',
-                            style: TextStyle(
-                              color: MyColors.white,
-                              fontSize: 10,
-                            ),
-                          ),
-                        ),
-                      ],
+                    const SizedBox(width: MySizes.spaceBtwItems),
+                    Expanded(
+                      child: Text(
+                        'Converse Chuck Taylor All Star',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    const SizedBox(width: MySizes.spaceBtwItems),
+                    IconButton(
+                      onPressed: () {
+                        // Action for the edit button
+                      },
+                      icon: const Icon(Iconsax.edit, color: MyColors.primary),
                     ),
                   ],
-                ),
-                trailing: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Iconsax.edit, color: MyColors.primary),
                 ),
               ),
             ),
